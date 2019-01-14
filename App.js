1ea8af652697d60bcbf5443fa5e2275fb09edf70
Andrés Zamora <andreszamora1998@gmail.com>
2019-01-13 20:02:04 -0500

    Adicion de Lista de Sugerencias

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
  import Home from './src/screens/containers/home'
  import Header from './src/sections/components/header'
+ import SuggestionList from './src/videos/containers/suggestion-list';
 type Props = {};
 export default class App extends Component<Props> {
   render() {
@@ -18,7 +19,7 @@ export default class App extends Component<Props> {
       <Header/>
       <Text>Buscador</Text>
       <Text>categorias</Text>
-      <Text>sugerencias</Text>
+      <SuggestionList/>
       </Home>
       
     );
