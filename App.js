9d93bd42cba43a5380344d821f91dced867bce0d
Andrés Zamora <andreszamora1998@gmail.com>
2019-01-17 22:45:16 -0500

    Peticiones con fetch

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
- import Home from './src/screens/containers/home'
- import Header from './src/sections/components/header'
- import SuggestionList from './src/videos/containers/suggestion-list';
+import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
+import SuggestionList from './src/videos/containers/suggestion-list';
+import API from './utils/api';
+
 type Props = {};
 export default class App extends Component<Props> {
+  async componentDidMount() {
+    const movies = await API.getSuggestion(10);
+    console.log(movies);
+  }
   render() {
     return (
       <Home>
-      <Header/>
-      <Text>Buscador</Text>
-      <Text>categorias</Text>
-      <SuggestionList/>
+        <Header />
+        <Text>buscador</Text>
+        <Text>categorías</Text>
+        <SuggestionList />
       </Home>
-      
     );
   }
 }
