ea0ed9f6ceb50a4ba54bbb27f25b3821be9bc4cf
Andrés Zamora <andreszamora1998@gmail.com>
2019-03-07 16:38:48 -0500

    WebView y ScrollView

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import Header from './sections/components/header';
 import SuggestionList from './videos/containers/suggestion-list';
 import CategoryList from './videos/containers/category-list.js';
 import Movie from './screens/containers/movie';
+import Search from './sections/containers/search';
 import {
   Text
 } from 'react-native';
@@ -34,7 +35,7 @@ class AppLayout extends Component {
 		return(
 		  <Home>
             <Header />
-            <Text>buscador</Text>
+            <Search/>
             <CategoryList />
             <SuggestionList />
           </Home>
---
+++ src/sections/containers/search.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {
+	TextInput,
+	StyleSheet
+} from 'react-native';
+
+class Search extends Component{
+	render(){
+		return(
+		<TextInput
+		placeholder="Busca tu Pelicula Favorita"
+		/>
+	)
+}
+
+}
+export default Search;
